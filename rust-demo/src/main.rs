0ba71b11a34975912308b57bcb4910d28bc04016
David Bohunek <david.bohunek@sky.uk>
2018-06-09 11:42:57 +0100

    rust demo improved

--- rust-demo/src/main.rs
+++ rust-demo/src/main.rs
@@ -5,12 +5,24 @@ fn main() {
 
     f1_laps_core::initialise("../_data-storage".to_string());
 
-    f1_laps_core::replay_all_laps();
+    let metadata = f1_laps_core::get_all_laps_metadata();
+    println!("metadata >> {:?}", metadata);
+
+    let lap_data = f1_laps_core::get_lap_data(metadata[0].identifier.clone());
+    println!("lap data len >> {:?}", lap_data.len());
+
+    f1_laps_core::replay_lap(metadata[0].identifier.clone());
+
+    // f1_laps_core::replay_all_laps();
     // f1_laps_core::start_listening(20777, true);
 
+    subscribe_to_ticks();
+}
+
+fn subscribe_to_ticks() {
     loop {
         if let Some(tick) = f1_laps_core::get_next_tick() {
-            println!(">> {:?}", tick.live_data.current_speed);
+            println!("tick >> {:?}", tick.live_data.current_speed);
         }
         std::thread::park_timeout(std::time::Duration::from_millis(30));
     }
