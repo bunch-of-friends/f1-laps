aea9afd7e3d1932bf23328d1d2cc1ed7c2d7f82d
David Bohunek <david.bohunek@sky.uk>
2018-07-14 12:40:19 +0100

    no more unsafe

---
+++ core/src/aggregation/collector.rs
@@ -0,0 +1,55 @@
+use aggregation::tick::{Lap, LiveData, Sector, Session, Tick};
+
+pub struct Collector {
+    session: Option<Session>,
+    live_data: Option<LiveData>,
+    lap: Option<Lap>,
+    sector: Option<Sector>,
+}
+
+impl Collector {
+    pub fn new() -> Collector {
+        Collector {
+            session: None,
+            live_data: None,
+            lap: None,
+            sector: None,
+        }
+    }
+
+    pub fn has_data(&self) -> bool {
+        return self.live_data.is_some();
+    }
+
+    pub fn set_data(&mut self, tick: Tick) {
+        self.live_data = Some(tick.live_data);
+
+        if let Some(session) = tick.session_started {
+            self.session = Some(session);
+        }
+
+        if let Some(lap) = tick.lap_finished {
+            self.lap = Some(lap);
+        }
+
+        if let Some(sector) = tick.sector_finished {
+            self.sector = Some(sector);
+        }
+    }
+
+    pub fn get_data(&mut self) -> (LiveData, Option<Session>, Option<Lap>, Option<Sector>) {
+        let res = (
+            self.live_data.expect("live data not set"),
+            self.session,
+            self.lap,
+            self.sector,
+        );
+
+        self.live_data = None;
+        self.session = None;
+        self.lap = None;
+        self.sector = None;
+
+        return res;
+    }
+}
