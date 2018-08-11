b672335b084d4f9eb2e90155ff0576a54e3f73f4
Zhanzhan He <tomhe123@hotmail.com>
2018-08-11 10:47:30 +0100

    Make start script work from scratch (#29)

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 sh scripts/cleanup.sh
-sh scripts/install.sh
+sh scripts/install-modules.sh
 sh scripts/build-modules.sh
+sh scripts/install-apps.sh
 sh scripts/build-apps.sh
 sh scripts/run-desktop.sh
