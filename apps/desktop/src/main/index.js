915c3bebae3a4e5ecb8749d5ad543542071fb4e8
Zhanzhan He <zhanzhan.he@sky.uk>
2018-08-11 01:12:52 +0100

    Basic JS setup for electron-webpack

---
+++ apps/desktop/src/main/index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const f1 = require('f1-laps-js-bridge');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+function startApp() {
+    const window = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            preload: path.resolve(__dirname, 'preload.js')
+        }
+    });
+
+    if (isDevelopment) {
+        window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+        window.webContents.openDevTools();
+    } else {
+        window.loadURL(url.format({
+            pathname: path.resolve(__dirname, '../renderer/index.html'),
+            protocol: 'file',
+            slashes: true
+        }));
+    }
+}
+
+app.on('ready', startApp);
