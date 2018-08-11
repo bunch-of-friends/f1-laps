b689ed9cf679b93a8c43c818945980151b88f5a2
David Bohunek <david.bohunek@sky.uk>
2018-08-11 11:01:53 +0100

    expose entite core module

--- apps/desktop/src/main/preload.js
+++ apps/desktop/src/main/preload.js
@@ -2,8 +2,4 @@ const f1 = require('f1-laps-js-bridge');
 
 f1.initialise({ updateInterval: 30, storagePath: '../../_data-storage' });
 
-window.f1 = {
-    newSession: f1.newSession,
-    liveData: f1.liveData,
-    replayAllLaps: f1.replayAllLaps
-};
+window.f1 = f1;
