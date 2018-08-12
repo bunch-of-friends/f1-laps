22833ecb64d5225870ace82f5ccc35a5ecc796ee
David Bohunek <david.bohunek@sky.uk>
2018-08-12 18:52:50 +0100

    data structures wip

--- modules/core/src/pipeline/mod.rs
+++ modules/core/src/pipeline/mod.rs
@@ -1,4 +1,3 @@
-use aggregation::tick::Tick;
 use udp::packet::Packet;
 
 trait DataReceiver {
@@ -6,18 +5,81 @@ trait DataReceiver {
     fn receive(packet: Packet);
 }
 
-// state-less
 trait PipeLine {
-    fn process(entry: PipelineEntry) -> PipeLineOutput;
+    fn build_labels(entry: &PipelineEntry) -> PacketLabels;
+    fn build_stats(entry: &PipelineEntry, labels: PacketLabels) -> PacketStats;
+    fn build_context(entry: &PipelineEntry, labels: PacketLabels) -> Context;
+}
+
+struct InputTick {
+    pub session_time: f32,
+    pub session_distance: f32,
+    pub lap_time: f32,
+    pub lap_distance: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub speed: f32,
+    pub throttle: f32,
+    pub steer: f32,
+    pub brake: f32,
+    pub gear: f32,
+    pub lap_number: u8,
+    pub engine_rate: f32,
+    pub car_position: u8,
+    pub drs: bool,
+    pub sector: u8,
+    pub sector1_time: f32,
+    pub sector2_time: f32,
+    pub team_id: u8,
+    pub total_laps: u8,
+    pub last_lap_time: f32,
+    pub max_gears: u8,
+    pub session_type: u8,
+    pub track_id: u8,
+    pub vehicle_fia_flags: i8,
+    pub era: u16,
+    pub tyre_compound: u8, 
+    pub current_lap_invalid: bool,
+    pub is_spectating: bool,
+    
+    pub cars_total: u8,
 }
 
 struct PipelineEntry<'a> {
-    packet: &'a Packet,
+    input_tick: &'a InputTick,
     context: &'a Context,
 }
 
-struct PipeLineOutput {
-    tick: Tick,
+struct PacketLabels {
+    is_new_session: bool,
+    is_new_lap: bool,
+    is_new_sector: bool,
+}
+
+struct PacketStats {
+    previous_lap: Option<FinishedLap>,
+    previous_sector: Option<FinishedSector>,
+    session: Session,
+}
+
+struct FinishedLap {
+    number: u8,
+    sectors: [f32; 3],
+    tyre_compound: u8,
+}
+
+struct FinishedSector {
+    number: u8,
+    time: f32,
+    tyre_compound: u8,
+}
+
+struct Session {
+    pub track_id: u8,
+    pub session_type: u8,
+    pub team_id: u8,
+    pub era: u16,
 }
 
 struct Context {
@@ -25,8 +87,6 @@ struct Context {
     history_context: HistoryContext,
 }
 
-// in-memory
 struct SessionContext {}
 
-// fs
 struct HistoryContext {}
