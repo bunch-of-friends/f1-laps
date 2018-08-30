f3080c7558973ec05da31570db82505201c25976
David Bohunek <david.bohunek@sky.uk>
2018-08-30 20:00:28 +0100

    enums updated

--- modules/js-bridge/src/types.ts
+++ modules/js-bridge/src/types.ts
@@ -15,149 +15,71 @@ export interface Tick {
     carMotion: CarMotion;
 }
 
-// export enum DRS {
-//     Off = 0,
-//     On
-// }
+export interface SessionIdentifier {
+    track_id: Track;
+    session_type: SessionType;
+    era: Era;
+    session_uid: number;
+    session_time: number;
+}
 
-// export enum TractionControl {
-//     Off = 0,
-//     Medium,
-//     High
-// }
+export interface Lap {
+    lap_number: number;
+    lap_time: number;
+    sector_times: Array<number>;
+    is_finished: boolean;
+}
 
-// export enum ABS {
-//     Off = 0,
-//     On
-// }
+export interface Sector {
+    sector_number: number;
+    sector_time: number;
+    is_finished: boolean;
+}
 
-// export enum PitStatus {
-//     None = 0,
-//     Pitting,
-//     InPitArea
-// }
+export interface SessionData {
+    weather: Weather;
+    era: Era;
+    session_type: SessionType;
+    track_id: Track;
+    track_temperature: number;
+    air_temperature: number;
+    race_laps: number;
+    track_lenght: number;
+    session_time_left: number;
+    session_duration: number;
+    is_game_paused: boolean;
+    is_spectating: boolean;
+    is_online_game: boolean;
+    safety_car_status: SafetyCarStatus;
+}
 
-// export enum Sector {
-//     One = 0,
-//     Two,
-//     Three
-// }
-
-// export enum ModernTeam {
-//     Redbull = 0,
-//     Ferrari = 1,
-//     McLaren = 2,
-//     Renault = 3,
-//     Mercedes = 4,
-//     Sauber = 5,
-//     ForceIndia = 6,
-//     Williams = 7,
-//     ToroRosso = 8,
-//     Haas = 11
-// }
-
-// export enum ClassicTeam {
-//     Williams1992 = 0,
-//     McLaren1988,
-//     McLaren2008,
-//     Ferrari2004,
-//     Ferrari1995,
-//     Ferrari2007,
-//     McLaren1998,
-//     Williams1996,
-//     Renault2006,
-//     Ferrari2002,
-//     Redbull2010,
-//     McLaren1991
-// }
-
-// export type Team = ClassicTeam | ModernTeam;
-
-// export enum SessionType {
-//     Unknown = 0,
-//     Practice,
-//     Qualifying,
-//     Race
-// }
-
-// export enum DRSAllowed {
-//     NotAllowed = 0,
-//     Allowed,
-//     InvalidOrUnknown
-// }
-
-// export enum Track {
-//     Melbourne = 0,
-//     Sepang,
-//     Shanghai,
-//     Bahrain,
-//     Catalunya,
-//     Monaco,
-//     Montreal,
-//     Silverstone,
-//     Hockenheim,
-//     Hungaroring,
-//     Spa,
-//     Monza,
-//     Singapore,
-//     Suzuka,
-//     AbuDhabi,
-//     Texas,
-//     Brazil,
-//     Austria,
-//     Sochi,
-//     Mexico,
-//     Baku,
-//     BahrainShort,
-//     SilverstoneShort,
-//     TexasShort,
-//     SuzukaShort
-// }
-
-// export enum VehicleFIAFlags {
-//     InvalidOrUnknown = -1,
-//     None,
-//     Green,
-//     Blue,
-//     Yellow,
-//     Red
-// }
-
-// export enum Era {
-//     Modern = 2017,
-//     Classic = 1980
-// }
-
-// export enum TyreCompound {
-//     UltraSoft = 0,
-//     SuperSoft,
-//     Soft,
-//     Medium,
-//     Hard,
-//     Inter,
-//     Wet
-// }
-
-// export enum FuelMix {
-//     Lean = 0,
-//     Standard,
-//     Rich,
-//     Max
-// }
-
-// export interface LapMetadata {
-//     identifier: string;
-//     recordedDate: string;
-//     trackId: Track;
-//     teamId: Team;
-//     era: Era;
-//     tyreCompound: TyreCompound;
-//     sessionType: SessionType;
-//     lapNumber: number;
-//     lapTime: number;
-//     note: string;
-//     sectorTimes: Array<number>;
-// }
+export interface CarStatus {
+    traction_control: TrackionControl;
+    antilock_brakes: AntilockBrakes;
+    fuel_mix: FuelMix;
+    front_brake_bias: number;
+    pit_limiter_status: PitLimiterStatus;
+    fuel_in_tank: number;
+    fuel_capacity: number;
+    max_rpm: number;
+    max_gears: number;
+    is_drs_allowed: boolean;
+    tyres_wear: Array<number>;
+    tyre_compound: TyreCompound;
+    tyres_damage: Array<number>;
+    front_left_wing_damage: number;
+    front_right_wing_damage: number;
+    rear_wing_damage: number;
+    engine_damage: number;
+    gearbox_damage: number;
+    exhaust_damage: number;
+    flags: Flag;
+    ers_stored: number;
+    ers_mode: ErsMode;
+    ers_harvested_mghu: number;
+    ers_harvested_mghh: number;
+    ers_deployed: number;
+}
 
 export interface CarTelemetry {
     speed: number;
@@ -185,68 +107,153 @@ export interface CarMotion {
     g_force_vertical: number;
 }
 
-export interface CarStatus {
-    traction_control: number;
-    antilock_brakes: number;
-    fuel_mix: number;
-    front_brake_bias: number;
-    pit_limiter_status: number;
-    fuel_in_tank: number;
-    fuel_capacity: number;
-    max_rpm: number;
-    max_gears: number;
-    is_drs_allowed: boolean;
-    tyres_wear: Array<number>;
-    tyre_compound: number;
-    tyres_damage: Array<number>;
-    front_left_wing_damage: number;
-    front_right_wing_damage: number;
-    rear_wing_damage: number;
-    engine_damage: number;
-    gearbox_damage: number;
-    exhaust_damage: number;
-    flags: number;
-    ers_stored: number;
-    ers_mode: number;
-    ers_harvested_mghu: number;
-    ers_harvested_mghh: number;
-    ers_deployed: number;
+export enum Weather {
+    Clear = 0,
+    LightClourd = 1,
+    Overcast = 2,
+    LightRain = 3,
+    HeavyRain = 4,
+    Storm = 5
 }
 
-export interface SessionData {
-    weather: number;
-    era: number;
-    session_type: number;
-    track_id: number;
-    track_temperature: number;
-    air_temperature: number;
-    race_laps: number;
-    track_lenght: number;
-    session_time_left: number;
-    session_duration: number;
-    is_game_paused: boolean;
-    is_spectating: boolean;
-    is_online_game: boolean;
-    safety_car_status: number;
+export enum SessionType {
+    Unknown = 0,
+    P1 = 1,
+    P2 = 2,
+    P3 = 3,
+    ShortP = 4,
+    Q1 = 5,
+    Q2 = 6,
+    Q3 = 7,
+    ShortQ = 8,
+    OneShotQ = 9,
+    Race = 10,
+    Race2 = 11,
+    TT = 12
 }
 
-export interface SessionIdentifier {
-    track_id: number;
-    session_type: number;
-    era: number;
-    session_uid: number;
-    session_time: number;
+export enum TrackionControl {
+    Low = 0,
+    Medium = 1,
+    High = 2
 }
 
-export interface Sector {
-    sector_number: number;
-    sector_time: number;
-    is_finished: boolean;
+export enum AntilockBrakes {
+    Off = 0,
+    On = 1
 }
 
-export interface Lap {
-    lap_number: number;
-    lap_time: number;
-    sector_times: Array<number>;
-    is_finished: boolean;
+export enum FuelMix {
+    Lean = 0,
+    Standard = 1,
+    Rich = 2,
+    Max = 3
+}
+
+export enum PitLimiterStatus {
+    Off = 0,
+    On = 1
+}
+
+export enum TyreCompound {
+    HyperSoft = 0,
+    UltraSoft = 1,
+    SuperSoft = 2,
+    Soft = 3,
+    Medium = 4,
+    Hard = 5,
+    SuperHard = 6,
+    Inter = 7,
+    Wet = 8
+}
+
+export enum Flag {
+    Unknown = -1,
+    None = 0,
+    Green = 1,
+    Blue = 2,
+    Yellow = 3,
+    Red = 4
+}
+
+export enum ErsMode {
+    None = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Overtake = 4,
+    Hotlap = 5
+}
+
+export enum Era {
+    Modern = 0,
+    Classic = 1
+}
+
+export enum Track {
+    Unknown = -1,
+    Melbourne = 0,
+    PaulRircard = 1,
+    Shanghai = 2,
+    Sakhir = 3,
+    Catalunya = 4,
+    Monaco = 5,
+    Montreal = 6,
+    Silverstone = 7,
+    Hockenheim = 8,
+    Hungaroring = 9,
+    Spa = 10,
+    Monza = 11,
+    Singapore = 12,
+    Suzuka = 13,
+    AbuDhabi = 14,
+    Texas = 15,
+    Brazil = 16,
+    Austria = 17,
+    Sochi = 18,
+    Mexico = 19,
+    Baku = 20,
+    SakhirShort = 21,
+    SilverstoneShort = 22,
+    TexasShort = 23,
+    SuzukaShort = 24
+}
+
+export enum SafetyCarStatus {
+    NoSC = 0,
+    Full = 1,
+    VSC = 2
+}
+
+export enum Team {
+    Mercedes = 0,
+    Ferrari = 1,
+    RedBull = 2,
+    Williams = 3,
+    ForceIndia = 4,
+    Renault = 5,
+    ToroRosso = 6,
+    Haas = 7,
+    McLaren = 8,
+    Sauber = 9,
+    McLaren1988 = 10,
+    McLaren1991 = 11,
+    Williams1992 = 12,
+    Ferrari1995 = 13,
+    Williams1996 = 14,
+    McLaren1998 = 15,
+    Ferrari2002 = 16,
+    Ferrari2004 = 17,
+    Renault2006 = 18,
+    Ferrari2007 = 19,
+    McLaren2008 = 20,
+    RedBull2010 = 21,
+    Ferrari1976 = 22,
+    McLaren1976 = 34,
+    Lotus1972 = 35,
+    Ferrari1979 = 36,
+    McLaren1982 = 37,
+    Williams2003 = 38,
+    Brawn2009 = 39,
+    Lotus1978 = 40
 }
